740ddec4f2f922557906bebc51106c7e4c01e296
DavidAr55 <davidarturoloera@gmail.com>
2024-02-23 13:50:52 -0600

    Creating repository

---
+++ database/migrations/2024_02_23_194247_create_empleados_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            
+            $table->String('Nombre');
+            $table->String('ApellidoPaterno');
+            $table->String('ApellidoMaterno');
+            $table->String('Correo');
+            $table->String('Foto');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
